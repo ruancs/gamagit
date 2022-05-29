882c18a961429ea79b6ea99ab0bffe4cae706ad5
ruancs <ruan.cs@hotmail.com>
2022-05-28 23:47:23 -0300

    entendendo react routes

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Routes from './routes';
+
+
+
+function App() {
+
+  return (
+      
+    </>
+    
+  );
+}
+
+export default App;
+
+
+//html dentro de JS é chamado JSX
